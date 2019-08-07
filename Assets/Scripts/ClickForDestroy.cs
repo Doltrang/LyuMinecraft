fce77613828139751d04b9e492384d6053de8fd2
Alvin <lyualvin>
2019-08-07 14:33:31 +0800

    阿翔小魔王

---
+++ Assets/Scripts/ClickForDestroy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickForDestroy : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/DayNight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    public float Speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate( Vector3.up * Speed*Time.deltaTime, Space.Self);
+    }
+}
